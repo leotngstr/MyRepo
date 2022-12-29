1a35854af81697687415182de3e4ffc7c35fe8f8
LeoTang <108058799+leotngstr@users.noreply.github.com>
2022-12-29 18:50:16 +0700

    Delete expensetracker directory

--- expensetracker/backend/model.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-from pydantic import BaseModel, Field
-
-class Expense(BaseModel):
-    id: str
-    type: str
-    name: str
-    date: date
-    amount: int = Field(gt=0, description="The amount must be greater than 0")
